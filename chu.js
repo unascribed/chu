1fafbc5c5635c04a251fc0afcf1eacc29789fea0
Aesen Vismea <aesen@unascribed.com>
2017-05-31 18:44:36 -0400

    Add GitLab support

--- chu.js
+++ chu.js
@@ -14,114 +14,171 @@ server.use(restify.bodyParser());
 
 server.post(config.http.path, (req, res, next) => {
 	var channel = '#'+(req.query.channel || config.irc.homeChannel);
-	var event = req.header('X-GitHub-Event');
-	var action = req.body.action;
-	var repo = req.body.repository ? req.body.repository.full_name : null;
-	var org = req.body.organization ? req.body.organization.login : null;
-	var sender = req.body.sender.login;
-	if (event === 'issues') {
-		var issue = req.body.issue;
-		if (action === 'opened') {
-			msg(channel, repo, 'Issue #'+issue.number+' §a§lopened§r by '+sender, issue.title, issue.html_url);
-		} else if (action === 'closed') {
-			msg(channel, repo, 'Issue #'+issue.number+' §c§lclosed§r by '+sender, issue.title, issue.html_url);
-		} else if (action === 'reopened') {
-			msg(channel, repo, 'Issue #'+issue.number+' §e§lreopened§r by '+sender, issue.title, issue.html_url);
-		}
-	} else if (event === 'pull_request') {
-		var pr = req.body.pull_request;
-		if (action === 'opened') {
-			msg(channel, repo, 'Pull #'+pr.number+' §a§lopened§r by '+sender, pr.title, pr.html_url);
-		} else if (action === 'closed') {
-			if (req.body.pull_request.merged) {
-				msg(channel, repo, 'Pull #'+pr.number+' §d§lmerged§r by '+sender, pr.title, pr.html_url);
-			} else {
-				msg(channel, repo, 'Pull #'+pr.number+' §c§lclosed§r by '+sender, pr.title, pr.html_url);
+	if (req.header('X-GitHub-Event')) {
+		var event = req.header('X-GitHub-Event');
+		var action = req.body.action;
+		var repo = req.body.repository ? req.body.repository.full_name : null;
+		var org = req.body.organization ? req.body.organization.login : null;
+		var sender = req.body.sender.login;
+		if (event === 'issues') {
+			var issue = req.body.issue;
+			if (action === 'opened') {
+				msg(channel, repo, 'Issue #'+issue.number+' §a§lopened§r by '+sender, issue.title, issue.html_url);
+			} else if (action === 'closed') {
+				msg(channel, repo, 'Issue #'+issue.number+' §c§lclosed§r by '+sender, issue.title, issue.html_url);
+			} else if (action === 'reopened') {
+				msg(channel, repo, 'Issue #'+issue.number+' §e§lreopened§r by '+sender, issue.title, issue.html_url);
 			}
-		} else if (action === 'reopened') {
-			msg(channel, repo, 'Pull #'+pr.number+' §e§lreopened§r by '+sender, pr.title, pr.html_url);
-		}
-	} else if (event === 'push') {
-		var branch = req.body.ref.slice(11)+'/';
-		if (branch === req.body.repository.default_branch) branch = '';
-		req.body.commits.forEach((commit) => {
-			msg(channel, repo, 'Commit §a§lpushed§r by '+sender, '§l'+branch+commit.id.slice(0, 8)+'§r '+commit.message.split('\n')[0], commit.url); 
-		});
-	} else if (event === 'repository') {
-		if (action === 'created') {
-			msg(channel, repo, 'Repository §a§lcreated§r by '+sender, req.body.repository.description, req.body.repository.html_url);
-		} else if (action === 'deleted') {
-			msg(channel, repo, 'Repository §c§ldeleted§r by '+sender, '', null);
-		}
-	} else if (event === 'organization') {
-		if (action === 'member_added') {
-			msg(channel, org, req.body.membership.user.login+' §c§ljoined§r', '', null);
-		} else if (action === 'member_removed') {
-			msg(channel, org, req.body.membership.user.login+' §c§lleft§r', '', null);
-		} else if (action === 'member_invited') {
-			msg(channel, org, req.body.invitation.login+' §d§linvited§r by '+sender, '', null);
-		}
-	} else if (event === "ping") {
-		msg(channel, req.body.hook.type === "Repository" ? req.body.repository.full_name : req.body.organization.login+'/*', 'Bot connected by '+sender, req.body.zen, null);
-		if (channel !== '#'+config.irc.homeChannel) {
-			msg('#'+config.irc.homeChannel, req.body.hook.type === "Repository" ? req.body.repository.full_name : req.body.organization.login+'/*', 'Bot connected to '+channel+' by '+sender, req.body.zen, null);
-		}
-	} else if (event === "gollum") {
-		req.body.pages.forEach((page) => {
-			if (page.action === "created") {
-				msg(channel, repo, 'Wiki page §a§lcreated§r by '+sender, page.title, page.html_url); 
-			} else if (page.action === "edited") {
-				msg(channel, repo, 'Wiki page §e§lmodified§r by '+sender, page.title, page.html_url); 
+		} else if (event === 'pull_request') {
+			var pr = req.body.pull_request;
+			if (action === 'opened') {
+				msg(channel, repo, 'Pull #'+pr.number+' §a§lopened§r by '+sender, pr.title, pr.html_url);
+			} else if (action === 'closed') {
+				if (req.body.pull_request.merged) {
+					msg(channel, repo, 'Pull #'+pr.number+' §d§lmerged§r by '+sender, pr.title, pr.html_url);
+				} else {
+					msg(channel, repo, 'Pull #'+pr.number+' §c§lclosed§r by '+sender, pr.title, pr.html_url);
+				}
+			} else if (action === 'reopened') {
+				msg(channel, repo, 'Pull #'+pr.number+' §e§lreopened§r by '+sender, pr.title, pr.html_url);
 			}
-		});
-	} else if (event === "release") {
-		msg(channel, repo, 'Release §a§lpublished§r by '+sender, req.body.release.name || req.body.release.tag_name, req.body.release.html_url); 
-	} else if (event === "create") {
-		if (req.body.ref_type === "branch") {
-			var ref = req.body.ref.replace("refs/heads/", "");
-			msg(channel, repo, 'Branch §a§lcreated§r by '+sender, ref, req.body.repository.html_url+"/tree/"+ref); 
+		} else if (event === 'push') {
+			var branch = req.body.ref.slice(11)+'/';
+			if (branch === req.body.repository.default_branch) branch = '';
+			req.body.commits.forEach((commit) => {
+				msg(channel, repo, 'Commit §a§lpushed§r by '+sender, '§l'+branch+commit.id.slice(0, 8)+'§r '+commit.message.split('\n')[0], commit.url);
+			});
+		} else if (event === 'repository') {
+			if (action === 'created') {
+				msg(channel, repo, 'Repository §a§lcreated§r by '+sender, req.body.repository.description, req.body.repository.html_url);
+			} else if (action === 'deleted') {
+				msg(channel, repo, 'Repository §c§ldeleted§r by '+sender, '', null);
+			}
+		} else if (event === 'organization') {
+			if (action === 'member_added') {
+				msg(channel, org, req.body.membership.user.login+' §c§ljoined§r', '', null);
+			} else if (action === 'member_removed') {
+				msg(channel, org, req.body.membership.user.login+' §c§lleft§r', '', null);
+			} else if (action === 'member_invited') {
+				msg(channel, org, req.body.invitation.login+' §d§linvited§r by '+sender, '', null);
+			}
+		} else if (event === "ping") {
+			msg(channel, req.body.hook.type === "Repository" ? req.body.repository.full_name : req.body.organization.login+'/*', 'Bot connected by '+sender, req.body.zen, null);
+			if (channel !== '#'+config.irc.homeChannel) {
+				msg('#'+config.irc.homeChannel, req.body.hook.type === "Repository" ? req.body.repository.full_name : req.body.organization.login+'/*', 'Bot connected to '+channel+' by '+sender, req.body.zen, null);
+			}
+		} else if (event === "gollum") {
+			req.body.pages.forEach((page) => {
+				if (page.action === "created") {
+					msg(channel, repo, 'Wiki page §a§lcreated§r by '+sender, page.title, page.html_url);
+				} else if (page.action === "edited") {
+					msg(channel, repo, 'Wiki page §e§lmodified§r by '+sender, page.title, page.html_url);
+				}
+			});
+		} else if (event === "release") {
+			msg(channel, repo, 'Release §a§lpublished§r by '+sender, req.body.release.name || req.body.release.tag_name, req.body.release.html_url);
+		} else if (event === "create") {
+			if (req.body.ref_type === "branch") {
+				var ref = req.body.ref.replace("refs/heads/", "");
+				msg(channel, repo, 'Branch §a§lcreated§r by '+sender, ref, req.body.repository.html_url+"/tree/"+ref);
+			}
+		} else if (event === "delete") {
+			if (req.body.ref_type === "branch") {
+				var ref = req.body.ref.replace("refs/heads/", "");
+				msg(channel, repo, 'Branch §c§ldeleted§r by '+sender, ref, req.body.repository.html_url+"/tree/"+ref);
+			}
+		} else if (event === "team") {
+			if (action === "created") {
+				msg(channel, org, 'Team §a§lcreated§r by '+sender, req.body.team.name, null);
+			} else if (action === "deleted") {
+				msg(channel, org, 'Team §c§ldeleted§r by '+sender, req.body.team.name, null);
+			} else if (action === "edited") {
+				msg(channel, org, 'Team §e§ledited§r by '+sender, req.body.team.name, null);
+			} else if (action === "added_to_repository") {
+				msg(channel, repo, 'Team §a§ladded§r to repository by '+sender, req.body.team.name, null);
+			} else if (action === "removed_from_repository") {
+				msg(channel, repo, 'Team §c§lremoved§r from repository by '+sender, req.body.team.name, null);
+			}
+		} else if (event === 'issue_comment') {
+			var issue = req.body.issue;
+			var comment = req.body.comment;
+			if (action === 'created') {
+				msg(channel, repo, 'Comment on issue #'+issue.number+' §a§lposted§r by '+sender, issue.title, comment.html_url);
+			} else if (action === 'deleted') {
+				msg(channel, repo, 'Comment on issue #'+issue.number+' §c§ldeleted§r by '+sender, issue.title, comment.html_url);
+			} else if (action === 'edited') {
+				msg(channel, repo, 'Comment on issue #'+issue.number+' §e§ledited§r by '+sender, issue.title, comment.html_url);
+			}
+		} else if (event === 'status') {
+			var branch = req.body.branches[0].name+'/';
+			if (branch === req.body.repository.default_branch) branch = '';
+			var state = req.body.state;
+			var commit = req.body.sha.slice(0, 8);
+			if (state === 'success') {
+				msg(channel, repo, 'Build §a§lsuccessful§r!', '§l'+branch+commit+'§r');
+			} else if (state === 'failure') {
+				msg(channel, repo, 'Build §c§lfailed§r.', '§l'+branch+commit+'§r');
+			} else if (state === 'error') {
+				msg(channel, repo, 'Build §7§lerrored§r!', '§l'+branch+commit+'§r');
+			}
 		}
-	} else if (event === "delete") {
-		if (req.body.ref_type === "branch") {
-			var ref = req.body.ref.replace("refs/heads/", "");
-			msg(channel, repo, 'Branch §c§ldeleted§r by '+sender, ref, req.body.repository.html_url+"/tree/"+ref); 
+		res.send(200, 'OK');
+	} else if (req.header('X-Gitlab-Event')) {
+		var repo = req.body.project ? req.body.project.namespace+"/"+req.body.project.name : null;
+		var event = req.header('X-Gitlab-Event');
+		if (event === "Push Hook") {
+			var senderId = req.body.user_id;
+			request.get(config.gitlab.url+'users/'+senderId+'?private_token='+config.gitlab.token, (err, res, body) => {
+				if (res && res.statusCode == 200) {
+					var sender = JSON.parse(body).username;
+					var branch = req.body.ref.slice(11)+'/';
+					if (branch === req.body.project.default_branch+'/') branch = '';
+					req.body.commits.forEach((commit) => {
+						msg(channel, repo, 'Commit §a§lpushed§r by '+sender, '§l'+branch+commit.id.slice(0, 8)+'§r '+commit.message.split('\n')[0], null, true);
+					});
+				} else {
+					console.error("GitLab API returned "+res.statusCode);
+				}
+			});
+		} else if (event === "Note Hook") {
+			var sender = req.body.user.username;
+			var objectType = req.body.object_attributes.noteable_type;
+			if (objectType === "Commit") {
+				msg(channel, repo, 'Comment on commit '+req.body.commit.id.slice(0, 8)+' §a§lposted§r by '+sender, req.body.commit.message.split('\n')[0], null, true);
+			} else if (objectType === "MergeRequest") {
+				msg(channel, repo, 'Comment on merge request #'+req.body.merge_request.id+' §a§lposted§r by '+sender, req.body.merge_request.title, null, true);
+			} else if (objectType === "Issue") {
+				msg(channel, repo, 'Comment on issue #'+req.body.issue.iid+' §a§lposted§r by '+sender, req.body.issue.title, null, true);
+			} else if (objectType === "Snippet") {
+				msg(channel, repo, 'Comment on snippet #'+req.body.snippet.iid+' §a§lposted§r by '+sender, req.body.snippet.title, null, true);
+			}
+		} else if (event === "Merge Request Hook") {
+			var sender = req.body.user.username;
+			var issue = req.body.object_attributes;
+			var action = issue.action;
+			if (action === 'open') {
+				msg(channel, repo, 'Issue #'+issue.iid+' §a§lopened§r by '+sender, issue.title, false, true);
+			} else if (action === 'close') {
+				msg(channel, repo, 'Issue #'+issue.iid+' §c§lclosed§r by '+sender, issue.title, false, true);
+			} else if (action === 'reopen') {
+				msg(channel, repo, 'Issue #'+issue.iid+' §e§lreopened§r by '+sender, issue.title, false, true);
+			}
+		} else if (event === "Issue Hook") {
+			var sender = req.body.user.username;
+			var issue = req.body.object_attributes;
+			var action = issue.action;
+			if (action === 'open') {
+				msg(channel, repo, 'Issue #'+issue.iid+' §a§lopened§r by '+sender, issue.title, false, true);
+			} else if (action === 'close') {
+				msg(channel, repo, 'Issue #'+issue.iid+' §c§lclosed§r by '+sender, issue.title, false, true);
+			} else if (action === 'reopen') {
+				msg(channel, repo, 'Issue #'+issue.iid+' §e§lreopened§r by '+sender, issue.title, false, true);
+			}
 		}
-	} else if (event === "team") {
-		if (action === "created") {
-			msg(channel, org, 'Team §a§lcreated§r by '+sender, req.body.team.name, null); 
-		} else if (action === "deleted") {
-			msg(channel, org, 'Team §c§ldeleted§r by '+sender, req.body.team.name, null); 
-		} else if (action === "edited") {
-			msg(channel, org, 'Team §e§ledited§r by '+sender, req.body.team.name, null); 
-		} else if (action === "added_to_repository") {
-			msg(channel, repo, 'Team §a§ladded§r to repository by '+sender, req.body.team.name, null); 
-		} else if (action === "removed_from_repository") {
-			msg(channel, repo, 'Team §c§lremoved§r from repository by '+sender, req.body.team.name, null); 
-		}
-	} else if (event === 'issue_comment') {
-		var issue = req.body.issue;
-		var comment = req.body.comment;
-		if (action === 'created') {
-			msg(channel, repo, 'Comment on issue #'+issue.number+' §a§lposted§r by '+sender, issue.title, comment.html_url);
-		} else if (action === 'deleted') {
-			msg(channel, repo, 'Comment on issue #'+issue.number+' §c§ldeleted§r by '+sender, issue.title, comment.html_url);
-		} else if (action === 'edited') {
-			msg(channel, repo, 'Comment on issue #'+issue.number+' §e§ledited§r by '+sender, issue.title, comment.html_url);
-		}
-	} else if (event === 'status') {
-		var branch = req.body.branches[0].name+'/';
-		if (branch === req.body.repository.default_branch) branch = '';
-		var state = req.body.state;
-		var commit = req.body.sha.slice(0, 8);
-		if (state === 'success') {
-			msg(channel, repo, 'Build §a§lsuccessful§r!', '§l'+branch+commit+'§r');
-		} else if (state === 'failure') {
-			msg(channel, repo, 'Build §c§lfailed§r.', '§l'+branch+commit+'§r');
-		} else if (state === 'error') {
-			msg(channel, repo, 'Build §7§lerrored§r!', '§l'+branch+commit+'§r');
-		}
+		res.send(200, 'OK');
+	} else {
+		res.send(404);
 	}
-	res.send(200, 'OK');
 });
 
 server.listen(config.http.port, config.http.bind);
@@ -153,16 +210,16 @@ var mcCodes = {
 	"§r": "\x0F", // reset
 }
 
-function msg(channel, repo, msg, desc, url) {
-	console.log('[%s] %s: %s %s', repo, msg, desc, url);
-	if (url != null) {
+function msg(channel, repo, msg, desc, url, gitlab) {
+	console.log('[%s] %s: %s %s', repo, msg, desc, url || "");
+	if (url) {
 		request.post('https://git.io/create', {
 			form:{url:url}
 		}, (err, res, body) => {
-			client.say(channel, substituteColors(util.format(config.messageFormat, repo, msg, desc, err || (!res || res.statusCode != 200) ? url : 'https://git.io/'+body)));
+			client.say(channel, substituteColors(util.format((gitlab ? config.messageFormatGitlab : config.messageFormat), repo, msg, desc, err || (!res || res.statusCode != 200) ? url : 'https://git.io/'+body)));
 		});
 	} else {
-		client.say(channel, substituteColors(util.format(config.messageFormat, repo, msg, desc, '')));
+		client.say(channel, substituteColors(util.format((gitlab ? config.messageFormatGitlab : config.messageFormat), repo, msg, desc, '')));
 	}
 }
 
